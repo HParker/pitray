5c1366966071a1ca3b8eb9cac2af43ffb07b7741
HParker <ahess@westmont.edu>
2013-11-24 15:39:40 -0800

    init

---
+++ tray.py
@@ -0,0 +1,48 @@
+from bottle import run, route, static_file, template
+import os
+import urllib2
+
+root = os.path.expanduser('~/Music')
+
+@route('/')
+@route('/hello/<name>')
+def home(name="stranger"):
+    return "<h1>Hello ", name, "</h1>"
+
+def link_to(name, path):
+    if os.path.isdir(os.path.join(root, path)) == False:
+        return ("/play/"+path, name)
+    else:
+        return ("/show/"+path, name)
+
+def name(path):
+    return os.path.basename(path)
+
+
+def render(element):
+    return link_to(name(element), element)
+
+
+
+
+@route('/show/')
+@route('/show/<path:path>')
+def show_dir(path=''):
+    page = []
+    full_path = urllib2.unquote(os.path.join(root, path))
+    print '---------------------'
+    print full_path
+    print '---------------------'
+    for content in os.listdir(full_path):
+        page.append(render(os.path.join(path, content)))
+    return template('show_template', page=page)
+
+@route('/play/<path:path>')
+def play_file(path):
+    full_path = urllib2.unquote(os.path.join(root, path))
+    file_name = name(path)
+    print name(full_path), full_path
+    return static_file(file_name, root=full_path.replace(file_name, ''))
+
+if __name__ == "__main__":
+    run(host='localhost', port=8080, debug=True)
---
+++ views/show_template.tpl
@@ -0,0 +1,4 @@
+%for item in page:
+     <li>
+	<a href="{{item[0]}}"> {{item[1]}} </a>
+     </li>
